f02eec0299df3d72e2da71075fe96044ba312fd2
Nathan-Yinka <oludarenathaniel@gmail.com>
2023-09-12 20:04:14 +0100

    postresql add

--- myproject/settings.py
+++ myproject/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()
 
@@ -84,13 +85,13 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+db_from_env = dj_database_url.config(default="postgres://hng_api_user:99WwShBMhWEbGNqRAfZuyzUjtblUOXgH@dpg-ck0aom36fquc739kvk80-a.oregon-postgres.render.com/hng_api")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': db_from_env
 }
 
+# DATABASES["default"] = dj_database_url.config(default="postgres://hng_api_user:99WwShBMhWEbGNqRAfZuyzUjtblUOXgH@dpg-ck0aom36fquc739kvk80-a.oregon-postgres.render.com/hng_api")
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
