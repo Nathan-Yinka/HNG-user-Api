b0b43387d118091c29da39bc758156a0d6a406da
Nathan-Yinka <oludarenathaniel@gmail.com>
2023-09-12 14:56:05 +0100

    url editted

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("api",views.person_list,name="person"),
+    path("api/",views.person_list,name="person1"),
     path("api/<str:pk>",views.person_detail,name="detail")
 ]
