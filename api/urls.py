ec8ede0cb47d2bcb3c3a5f28ef00ad75dff72318
Nathan-Yinka <oludarenathaniel@gmail.com>
2023-09-12 14:49:25 +0100

    url editted

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.person_list,name="person"),
-    path("<str:pk>",views.person_detail,name="detail")
+    path("api",views.person_list,name="person"),
+    path("api/<str:pk>",views.person_detail,name="detail")
 ]
